e2069ef1b13b7fe3129afa230ecb4227e17b6e63
Amal Joy Chemparathy <amaljoyc@gmail.com>
2017-08-05 22:00:37 +0200

    full query list in offer page

--- src/offer/views.py
+++ src/offer/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.views import generic
+from query.models import Query
 
 
-class Offer(LoginRequiredMixin, generic.TemplateView):
+class Offer(LoginRequiredMixin, generic.ListView):
+    model = Query
+    paginate_by = 10
+    context_object_name = "queries"
     template_name = "offer/offer.html"
     http_method_names = ['get']
 
     def get(self, request, *args, **kwargs):
         return super(Offer, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return super(Offer, self).get_queryset().order_by('-id')
